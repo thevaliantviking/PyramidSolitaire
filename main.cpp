006ef92840ed81945d8163317a0073511be38290
thevaliantviking <victorlundbergiv@gmail.com>
2026-02-27 21:56:46 -0500

    Update main.cpp
    added force for UTF-8 symbols for windows machines

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "Pyramid.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 using namespace std;
 
 int main() {
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);  //Set console to UTF-8
+    #endif                        //for symbols
+   
     cout << "Building the Pyramid...." << endl;
     
     Pyramid game;
